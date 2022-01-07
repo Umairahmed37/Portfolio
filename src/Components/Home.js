1b9e3ac5aed11fe8f80d07b5299a1574574d0262
Umairahmed37 <emailumair125125@gmail.com>
2022-01-07 14:03:32 +0500

    donefront

---
+++ src/Components/Home.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
+
+
+const Home = () => {
+  return (
+    <div >
+      <div id="frontpage" className="pt-5 home  justify-content-evenly">
+        <div className="row container">
+
+          <div className="col-md-7 stack">
+            <h1>
+              Hey, i'm Full <br /> <span>Stack Developer</span>
+            </h1>
+            <p>hi! I'm Umair, a professional web developer from Pakistan<br />
+              I help brands convert their ideas into reality.
+            </p>
+
+            <div className="d-flex frontbuttons">
+
+              <div className="col-md-3" id="parenthire">
+                <Link to="/">
+                  <div id="hireme">
+                    <i class="fas fa-user-plus mx-2"></i>Hire me</div>
+                </Link>
+              </div>
+              <div id="frontdownload" className="col-md-3 align-self-center btn-danger " >
+                <i className="fas fa-download mx-2"></i>
+                Download CV</div>
+            </div>
+
+          </div>
+          <div className="col-md-3 profile">
+            <img className="" src={require("../Components/Images/profile2.png")} alt="portfolio image" />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        
+       </div>
+
+
+
+
+
+
+    </div>
+
+  )
+}
+
+export default Home
---
+++ src/Components/Navbar.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Button, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+
+
+
+const Navbar = () => {
+
+  const [shownav, setShownav] = useState(false)
+
+  const changenav = () => {
+    if (window.scrollY > 30) {
+      setShownav(true)
+    } else {
+      setShownav(false)
+    }
+  }
+  window.addEventListener('scroll', changenav)
+
+  return (
+    <div >
+
+      <nav id={shownav ? 'scrolling' : 'notscrolling'} className="navbar navbar-light navbar-expand-md fixed-top">
+        <div className="container">
+          <Link to="/" className="navbar-brand"> <Button className="contactbutton mt-2">
+            <i class="fas fa-paper-plane mx-2"></i>
+            Contact Me
+            </Button>
+          </Link>
+
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+              <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+
+
+            <div className="offcanvas-body d-inline-flex">
+              <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
+
+                <li className="nav-item">
+                  <Nav.Link as={Link} to="/" className="nav-link active" aria-current="page" >Home</Nav.Link>
+                </li>
+
+                <li className="nav-item">
+                  <Nav.Link as={Link} to="/Contact" className="nav-link" >Projects</Nav.Link>
+                </li>
+
+                <li className="nav-item">
+                  <Nav.Link as={Link} to="/Contact" className="nav-link" >Services</Nav.Link>
+                </li>
+                <li className="nav-item">
+                  <Nav.Link as={Link} to="/About" className="nav-link" >About Me</Nav.Link>
+                </li>
+
+              </ul>
+              <div className="justify-content-end align-self-center ">
+                <i className="mx-3 fa-brands fa-instagram"></i>
+                <i className="mx-3 fa-brands fa-twitter"></i>
+                <i className="mx-3 fa-brands fa-facebook"></i>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </nav >
+    </div>
+  )
+}
+
+export default Navbar
